80f9a12f12b75fa3c562e1f98c05dfa50289ea73
Francisco Javier Campos Pérez <u9770489609@gmail.com>
2026-01-12 14:15:54 +0100

    Segundo commit: Modificado mensaje de salida

--- src/com/ejemplo/practica/Main.java
+++ src/com/ejemplo/practica/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Tu Nombre y apellidos");
         System.out.println("Proyecto Git inicial funcionando correctamente");
+        System.out.println("Este es un cambio que se va a ver reflejado en el segundo commit");
     }
 }
 
