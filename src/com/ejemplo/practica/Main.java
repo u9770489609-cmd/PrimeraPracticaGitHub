c59d53af21334324e12c99d77569d43b5457b9eb
Francisco Javier Campos Pérez <u9770489609@gmail.com>
2026-01-12 13:22:52 +0100

    Proyecto Java inicial

---
+++ src/com/ejemplo/practica/Main.java
@@ -0,0 +1,10 @@
+package com.ejemplo.practica;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Tu Nombre y apellidos");
+        System.out.println("Proyecto Git inicial funcionando correctamente");
+    }
+}
+
